48160ccff19f11045dd3b4e6a9197a6c2748c83d
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2024-10-05 12:26:49 -0300

    Faltan checkear los testgit add .

--- tdas/diccionario/diccionario_hash.go
+++ tdas/diccionario/diccionario_hash.go
@@ -14,6 +14,7 @@ const (
 	PRIMERA_POSICION    = 0
 	FACTOR_REDIMENSION  = 3
 	FACTOR_CARGA_MAXIMO = 0.7
+	POSICION_INICIAL    = 0
 )
 
 type estado int
@@ -38,7 +39,7 @@ func CrearHash[K comparable, V any]() Diccionario[K, V] {
 
 func (hash *hashCerrado[K, V]) Guardar(clave K, dato V) {
 	bytes := convertirABytes(clave)
-	posicion := int(Hash32(bytes))
+	posicion := int(Hash32(bytes)) % hash.tam
 
 	// Checkear si es necesario redimensionar la tabla
 	if hash.factorCarga() > FACTOR_CARGA_MAXIMO {
@@ -47,7 +48,7 @@ func (hash *hashCerrado[K, V]) Guardar(clave K, dato V) {
 
 	for hash.tabla[posicion].estado == OCUPADO {
 		if posicion == hash.tam-1 {
-			posicion = PRIMERA_POSICION
+			posicion = PRIMERA_POSICION - 1
 		}
 		posicion++
 	}
@@ -58,46 +59,67 @@ func (hash *hashCerrado[K, V]) Guardar(clave K, dato V) {
 }
 
 func (hash *hashCerrado[K, V]) Pertenece(clave K) bool {
-	bytes := convertirABytes(clave)
-	posicion := int(Hash32(bytes)) % hash.tam
-	for 
-	return hash.tabla[posicion].estado == OCUPADO
+	return hash.buscarElemento(clave) != -1
 }
 
 func (hash *hashCerrado[K, V]) Obtener(clave K) V {
-	return hash.tabla[0].dato
+	posicion := hash.buscarElemento(clave)
+	if posicion == -1 {
+		panic("La clave no pertenece al diccionario")
+	}
+	return hash.tabla[posicion].dato
 }
 
 func (hash *hashCerrado[K, V]) Borrar(clave K) V {
-	return
+	posicion := hash.buscarElemento(clave)
+	if posicion == -1 {
+		panic("La clave no pertenece al diccionario")
+	}
+	valor := hash.tabla[posicion].dato
+	hash.tabla[posicion].estado = BORRADO
+	return valor
 }
 
 func (hash *hashCerrado[K, V]) Cantidad() int {
 	return hash.cantidad
 }
 
-func (hash *hashCerrado[K, V]) Iterar(func(clave K, dato V) bool) {
-	return
+func (hash *hashCerrado[K, V]) Iterar(f func(clave K, dato V) bool) {
+	for _, el := range hash.tabla {
+		if el.estado == OCUPADO {
+			if !f(el.clave, el.dato) {
+				break
+			}
+		}
+	}
 }
 
 func (hash *hashCerrado[K, V]) Iterador() IterDiccionario[K, V] {
-	return
+	return &iteradorHashCerrado[K, V]{hash: hash, posicionActual: POSICION_INICIAL}
 }
 
 // tda iterador
 type iteradorHashCerrado[K comparable, V any] struct {
-	Laura int
-	Va    int
+	hash           *hashCerrado[K, V]
+	posicionActual int
 }
 
-func (iterador *iteradorHashCerrado) HaySiguiente() bool {
-	return
+func (iterador *iteradorHashCerrado[K, V]) HaySiguiente() bool {
+	return iterador.posicionActual != iterador.hash.tam
 }
-func (iterador *iteradorHashCerrado) VerActual() (K, V) {
-	return
+func (iterador *iteradorHashCerrado[K, V]) VerActual() (K, V) {
+	if !iterador.HaySiguiente() {
+		panic("La clave no pertenece al diccionario")
+	}
+	celda := iterador.hash.tabla[iterador.posicionActual]
+	return celda.clave, celda.dato
+
 }
-func (iterador *iteradorHashCerrado) Siguiente() {
-	return
+func (iterador *iteradorHashCerrado[K, V]) Siguiente() {
+	if !iterador.HaySiguiente() {
+		panic("La clave no pertenece al diccionario")
+	}
+	iterador.posicionActual++
 }
 
 func convertirABytes[K comparable](clave K) []byte {
@@ -105,25 +127,31 @@ func convertirABytes[K comparable](clave K) []byte {
 }
 
 func (hash *hashCerrado[K, V]) redimensionar(tam int) {
-	nuevosDatos := make([]celdaHash[K, V], tam)
-	//for i := 0; i < hash.
-	hash.tabla = nuevosDatos
+	tablaAnterior := hash.tabla
+	hash.tabla = make([]celdaHash[K, V], tam)
 	hash.tam = tam
+	for _, elem := range tablaAnterior {
+		if elem.estado == OCUPADO {
+			hash.Guardar(elem.clave, elem.dato)
+		}
+	}
 }
 
 func (hash *hashCerrado[K, V]) factorCarga() float32 {
 	return (1 + float32(hash.cantidad) + float32(hash.borrados)) / float32(hash.tam)
 }
 
-func guardarElemento(celda celdaHash[K,V], tabla []celdaHash[K, V]){
-	for tabla.[posicion].estado == OCUPADO {
+func (hash *hashCerrado[K, V]) buscarElemento(clave K) int {
+	bytes := convertirABytes(clave)
+	posicion := int(Hash32(bytes)) % hash.tam
+	for hash.tabla[posicion].estado != VACIO {
+		if hash.tabla[posicion].clave == clave {
+			return posicion
+		}
 		if posicion == hash.tam-1 {
-			posicion = PRIMERA_POSICION
+			posicion = PRIMERA_POSICION - 1
 		}
 		posicion++
 	}
-	hash.tabla[posicion].clave = clave
-	hash.tabla[posicion].dato = dato
-	hash.tabla[posicion].estado = OCUPADO
-	hash.cantidad++
+	return -1
 }
