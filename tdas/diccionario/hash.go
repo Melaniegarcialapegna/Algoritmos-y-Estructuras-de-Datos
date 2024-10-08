3212ac3ec94f781dd8309df53fee88edaea4758d
Mirko Saenz Valiente <saenzvalientemirko@gmail.com>
2024-10-08 20:57:33 -0300

    Ojota! Chaaran

--- tdas/diccionario/hash.go
+++ tdas/diccionario/hash.go
@@ -1,175 +1,209 @@
 package diccionario
 
-//https://github.com/tenfyzhong/cityhash/blob/master/city32.go
-
 import (
-	"encoding/binary"
+	"fmt"
 )
 
-// Some primes between 2^63 and 2^64 for various uses.
 const (
-	k0 uint64 = 0xc3a5c85c97cb3127
-	k1 uint64 = 0xb492b66fbe98f273
-	k2 uint64 = 0x9ae16a3b2f90404f
+	VACIO               = 0
+	OCUPADO             = 1
+	BORRADO             = 2
+	BORRADOS_INICIAL    = 0
+	CANTIDAD_INICIAL    = 0
+	FACTOR_CARGA_MAXIMO = 0.7
+	FACTOR_CARGA_MINIMO = 0.2
+	FACTOR_REDIMENSION  = 3
+	POSICION_INICIAL    = 0
+	PRIMERA_POSICION    = 0
+	TAM_INICIAL         = 11
+	CANTIDAD_MINIMA     = 1
 )
 
-// Magic numbers for 32-bit hashing. Copied from Murmur3.
-const (
-	c1 uint32 = 0xcc9e2d51
-	c2 uint32 = 0x1b873593
-)
+type estado int
 
-func bswap32(x uint32) uint32 {
-	return ((x & 0xff000000) >> 24) |
-		((x & 0x00ff0000) >> 8) |
-		((x & 0x0000ff00) << 8) |
-		((x & 0x000000ff) << 24)
+// celdaHash representa una celda de una tabla de hash.
+type celdaHash[K comparable, V any] struct {
+	clave  K
+	dato   V
+	estado estado
 }
 
-func fetch32(p []byte) uint32 {
-	return binary.LittleEndian.Uint32(p)
+// hashCerrado es la implementacion de una tabla de hash.
+type hashCerrado[K comparable, V any] struct {
+	tabla    []celdaHash[K, V]
+	cantidad int
+	tam      int
+	borrados int
 }
 
-// A 32-bit to 32-bit integer hash copied from Murmr3.
-func fmix(h uint32) uint32 {
-	h ^= h >> 16
-	h *= 0x85ebca6b
-	h ^= h >> 13
-	h *= 0xc2b2ae35
-	h ^= h >> 16
-	return h
+// CrearHash crea y devuelve un diccionario implementado con una tabla de Hash cerrada.
+func CrearHash[K comparable, V any]() Diccionario[K, V] {
+	celdas := make([]celdaHash[K, V], TAM_INICIAL)
+	return &hashCerrado[K, V]{tabla: celdas, cantidad: CANTIDAD_INICIAL, tam: TAM_INICIAL, borrados: BORRADOS_INICIAL}
 }
 
-func rotate32(val uint32, shift int) uint32 {
-	// Avoid shifting by 32: doing so yields and undefined result.
-	if shift == 0 {
-		return val
-	}
-	return (val >> uint32(shift)) | (val << (32 - uint32(shift)))
-}
+func (hash *hashCerrado[K, V]) Guardar(clave K, dato V) {
 
-func permute3(a, b, c uint32) (uint32, uint32, uint32) {
-	return c, a, b
-}
-
-func mur(a, h uint32) uint32 {
-	// Helper from Murmur3 for combining two 32-bit values.
-	a *= c1
-	a = rotate32(a, 17)
-	a *= c2
-	h ^= a
-	h = rotate32(h, 19)
-	return h*5 + 0xe6546b64
-}
-
-func hash32Len13to24(s []byte, length int) uint32 {
-	a := fetch32(s[(length>>1)-4:])
-	b := fetch32(s[4:])
-	c := fetch32(s[length-8:])
-	d := fetch32(s[length>>1:])
-	e := fetch32(s)
-	f := fetch32(s[length-4:])
-	h := uint32(length)
-	return fmix(mur(f, mur(e, mur(d, mur(c, mur(b, mur(a, h)))))))
-}
-
-func hash32Len0to4(s []byte, length int) uint32 {
-	b := uint32(0)
-	c := uint32(9)
-	for _, v := range s[:length] {
-		b = uint32(int64(b)*int64(c1) + int64(int8(v)))
-		c ^= b
-	}
-	return fmix(mur(b, mur(uint32(length), c)))
-}
-
-func hash32Len5to12(s []byte, length int) uint32 {
-	a := uint32(length)
-	b := uint32(length) * 5
-	c := uint32(9)
-	d := b
-	a += fetch32(s)
-	b += fetch32(s[length-4:])
-	c += fetch32(s[(length>>1)&4:])
-	return fmix(mur(c, mur(b, mur(a, d))))
-}
-
-// CityHash32 return 32-bit hash.
-func CityHash32(s []byte) uint32 {
-	length := len(s)
-	if length <= 4 {
-		return hash32Len0to4(s, length)
-	} else if length <= 12 {
-		return hash32Len5to12(s, length)
-	} else if length <= 24 {
-		return hash32Len13to24(s, length)
+	// En caso de ser necesario, redimesiona la tabla de hash
+	if hash.factorCarga(true) > FACTOR_CARGA_MAXIMO {
+		hash.redimensionar(hash.tam * FACTOR_REDIMENSION)
 	}
 
-	// len > 24
-	h := uint32(length)
-	g := c1 * uint32(length)
-	f := g
-	a0 := rotate32(fetch32(s[length-4:])*c1, 17) * c2
-	a1 := rotate32(fetch32(s[length-8:])*c1, 17) * c2
-	a2 := rotate32(fetch32(s[length-16:])*c1, 17) * c2
-	a3 := rotate32(fetch32(s[length-12:])*c1, 17) * c2
-	a4 := rotate32(fetch32(s[length-20:])*c1, 17) * c2
-	h ^= a0
-	h = rotate32(h, 19)
-	h = h*5 + 0xe6546b64
-	h ^= a2
-	h = rotate32(h, 19)
-	h = h*5 + 0xe6546b64
-	g ^= a1
-	g = rotate32(g, 19)
-	g = g*5 + 0xe6546b64
-	g ^= a3
-	g = rotate32(g, 19)
-	g = g*5 + 0xe6546b64
-	f += a4
-	f = rotate32(f, 19)
-	f = f*5 + 0xe6546b64
-	iters := (length - 1) / 20
-	for {
-		a0 := rotate32(fetch32(s)*c1, 17) * c2
-		a1 := fetch32(s[4:])
-		a2 := rotate32(fetch32(s[8:])*c1, 17) * c2
-		a3 := rotate32(fetch32(s[12:])*c1, 17) * c2
-		a4 := fetch32(s[16:])
-		h ^= a0
-		h = rotate32(h, 18)
-		h = h*5 + 0xe6546b64
-		f += a1
-		f = rotate32(f, 19)
-		f *= c1
-		g += a2
-		g = rotate32(g, 18)
-		g = g*5 + 0xe6546b64
-		h ^= a3 + a1
-		h = rotate32(h, 19)
-		h = h*5 + 0xe6546b64
-		g ^= a4
-		g = bswap32(g) * 5
-		h += a4 * 5
-		h = bswap32(h)
-		f += a0
-		f, h, g = permute3(f, h, g)
-		s = s[20:]
+	bytes := convertirABytes(clave)
+	posicion := int(CityHash32(bytes)) % hash.tam
 
-		iters--
-		if iters == 0 {
-			break
+	for hash.tabla[posicion].estado == OCUPADO {
+		//Si la clave ya existe reemplaza el dato
+		if hash.tabla[posicion].clave == clave {
+			hash.tabla[posicion].dato = dato
+			return
+		}
+		//Si llegamos a la ultima celda seguimos buscando desde la primera
+		if posicion == hash.tam-1 {
+			posicion = PRIMERA_POSICION - 1
+		}
+		posicion++
+	}
+	hash.tabla[posicion].clave = clave
+	hash.tabla[posicion].dato = dato
+	hash.tabla[posicion].estado = OCUPADO
+	hash.cantidad++
+}
+
+func (hash *hashCerrado[K, V]) Pertenece(clave K) bool {
+	return hash.buscarElemento(clave) != -1
+}
+
+func (hash *hashCerrado[K, V]) Obtener(clave K) V {
+	posicion := hash.buscarElemento(clave)
+	if posicion == -1 {
+		panic("La clave no pertenece al diccionario")
+	}
+	return hash.tabla[posicion].dato
+}
+
+func (hash *hashCerrado[K, V]) Borrar(clave K) V {
+	posicion := hash.buscarElemento(clave)
+	if posicion == -1 {
+		panic("La clave no pertenece al diccionario")
+	}
+	valor := hash.tabla[posicion].dato
+	hash.tabla[posicion].estado = BORRADO
+	hash.cantidad--
+	hash.borrados++
+
+	if hash.factorCarga(false) < FACTOR_CARGA_MINIMO && CANTIDAD_MINIMA < hash.cantidad {
+		hash.redimensionar(hash.tam / FACTOR_REDIMENSION)
+	}
+	// En caso de ser necesario, redimesiona la tabla de hash
+
+	return valor
+}
+
+func (hash *hashCerrado[K, V]) Cantidad() int {
+	return hash.cantidad
+}
+
+func (hash *hashCerrado[K, V]) Iterar(f func(clave K, dato V) bool) {
+	for _, el := range hash.tabla {
+		if el.estado == OCUPADO {
+			if !f(el.clave, el.dato) {
+				break
+			}
 		}
 	}
-	g = rotate32(g, 11) * c1
-	g = rotate32(g, 17) * c1
-	f = rotate32(f, 11) * c1
-	f = rotate32(f, 17) * c1
-	h = rotate32(h+g, 19)
-	h = h*5 + 0xe6546b64
-	h = rotate32(h, 17) * c1
-	h = rotate32(h+f, 19)
-	h = h*5 + 0xe6546b64
-	h = rotate32(h, 17) * c1
-	return h
+}
+
+func (hash *hashCerrado[K, V]) Iterador() IterDiccionario[K, V] {
+	iterador := &iteradorHashCerrado[K, V]{diccionario: hash, posicionActual: POSICION_INICIAL}
+	iterador.proximoOcupado()
+	return iterador
+}
+
+// iteradorHashCerrado representa un iterador para un hash cerrado.
+type iteradorHashCerrado[K comparable, V any] struct {
+	diccionario    *hashCerrado[K, V]
+	posicionActual int
+}
+
+func (iterador *iteradorHashCerrado[K, V]) HaySiguiente() bool {
+	return iterador.posicionActual < iterador.diccionario.tam
+}
+
+func (iterador *iteradorHashCerrado[K, V]) VerActual() (K, V) {
+	if !iterador.HaySiguiente() {
+		panic("El iterador termino de iterar")
+	}
+	return iterador.diccionario.tabla[iterador.posicionActual].clave, iterador.diccionario.tabla[iterador.posicionActual].dato
+}
+
+func (iterador *iteradorHashCerrado[K, V]) Siguiente() {
+	if !iterador.HaySiguiente() {
+		panic("El iterador termino de iterar")
+	}
+	iterador.posicionActual++
+	iterador.proximoOcupado()
+}
+
+// convertirABytes transforma un tipo de dato genérico a un array de bytes
+func convertirABytes[K comparable](clave K) []byte {
+	return []byte(fmt.Sprintf("%v", clave))
+}
+
+// redimensionar toma la tabla actual del hash y transfiere sus elementos a una nueva tabla de mayor tamaño
+func (hash *hashCerrado[K, V]) redimensionar(tam int) {
+	tablaAnterior := hash.tabla
+	//creamos una nueva tabla que reemplazara a la actual
+	hash.tabla = make([]celdaHash[K, V], tam)
+	hash.tam = tam
+	//reiniciamos los valores
+	hash.cantidad = CANTIDAD_INICIAL
+	hash.borrados = BORRADOS_INICIAL
+	//reubicamos los elementos en la nueva tabla
+	for _, elem := range tablaAnterior {
+		if elem.estado == OCUPADO {
+			hash.Guardar(elem.clave, elem.dato)
+		}
+	}
+}
+
+// factorCarga calcula el factor de carga de un hash y lo devuelve como float32
+func (hash *hashCerrado[K, V]) factorCarga(contarBorrados bool) float32 {
+	numerador := float32(hash.cantidad)
+	if contarBorrados {
+		numerador += float32(hash.borrados)
+	}
+	return numerador / float32(hash.tam)
+}
+
+// buscarElemento devuelve la posicion de una clave en una tabla de hash,en caso de no encontrarla devuelve -1
+func (hash *hashCerrado[K, V]) buscarElemento(clave K) int {
+	bytes := convertirABytes(clave)
+	posicion := int(CityHash32(bytes)) % hash.tam
+	//iteramos hasta hallar una celda que no esta vacia
+	for hash.tabla[posicion].estado != VACIO {
+		// chequeamos si encontramos la celda
+		if hash.tabla[posicion].estado == OCUPADO && hash.tabla[posicion].clave == clave {
+			return posicion
+		}
+		// en caso de estar en la ultima posicion, seguimos buscando a la primera
+		if posicion == hash.tam-1 {
+			posicion = PRIMERA_POSICION - 1
+		}
+		posicion++
+	}
+	// llegado aca, ya itero toda la tabla y no encontre un ocupado
+	return -1
+}
+
+// proximoOcupado incrementa la posicion del iterador hasta encontrar una celda ocupada y devuelve true,
+// en caso de no haber un proximo ocupado devuelve false
+func (iterador *iteradorHashCerrado[K, V]) proximoOcupado() bool {
+	for iterador.HaySiguiente() {
+		if iterador.diccionario.tabla[iterador.posicionActual].estado == OCUPADO {
+			return true
+		}
+		iterador.posicionActual++
+	}
+	return false
 }
