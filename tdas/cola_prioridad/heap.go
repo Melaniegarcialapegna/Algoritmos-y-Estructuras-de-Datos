4606f0e814bd548af0fa47f06ebc6a350d3de0e2
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2024-10-29 16:48:46 -0300

    heap

--- tdas/cola_prioridad/heap.go
+++ tdas/cola_prioridad/heap.go
@@ -1,3 +1,94 @@
 package cola_prioridad
 
-//Normal Rockwell
+// Normal Rockwell
+
+const (
+	FACTOR_REDIMENSION_AUMENTO     = 2
+	FACTOR_REDIMENSION_DISMINUCION = 2
+)
+
+type heap[T any] struct {
+	datos    []T
+	cantidad int
+	cmp      func(T, T) int
+}
+
+func CrearColaPriodad[T any](funcion_cmp func(T, T) int) ColaPrioridad[T] {
+	return &heap[T]{cmp: funcion_cmp}
+}
+
+func (heap *heap[T]) EstaVacia() bool {
+	return heap.cantidad == 0
+}
+
+func (heap *heap[T]) Encolar(valor T) {
+	if heap.cantidad == len(heap.datos) {
+		heap.redimensionar(heap.cantidad * FACTOR_REDIMENSION_AUMENTO)
+	}
+	heap.datos[heap.cantidad] = valor
+	heap.upHeap(heap.cantidad)
+	heap.cantidad++
+}
+
+func (heap *heap[T]) VerMax() T {
+	return heap.datos[0]
+}
+
+func (heap *heap[T]) Desencolar() T {
+	if heap.EstaVacia() {
+		panic("La cola esta vacia")
+	}
+	borrado := heap.datos[0]
+	heap.datos[0], heap.datos[heap.cantidad-1] = heap.datos[heap.cantidad-1], heap.datos[0]
+	heap.cantidad--
+	heap.downHeap(0)
+
+	return borrado
+}
+
+func (heap *heap[T]) Cantidad() int {
+	return heap.cantidad
+}
+
+func (heap *heap[T]) upHeap(indice int) {
+	for indice > 0 {
+		padre := (indice - 1) / 2
+		if heap.cmp(heap.datos[padre], heap.datos[indice]) > 0 {
+			break
+		}
+		//Swap
+		heap.datos[padre], heap.datos[indice] = heap.datos[indice], heap.datos[padre]
+		indice = padre
+	}
+}
+
+func (heap *heap[T]) downHeap(indice int) {
+	for indice < heap.cantidad {
+		hijoIzq := (2 * indice) + 1
+		hijoDer := (2 * indice) + 2
+
+		if hijoIzq >= heap.cantidad {
+			break
+		}
+
+		indiceHijo := hijoIzq
+		if (hijoDer < heap.cantidad) && (heap.cmp(heap.datos[hijoDer], heap.datos[indiceHijo]) > 0) {
+			indiceHijo = hijoDer
+		}
+
+		if heap.cmp(heap.datos[indiceHijo], heap.datos[indice]) < 0 {
+			break
+		}
+		//Swap
+		heap.datos[indiceHijo], heap.datos[indice] = heap.datos[indice], heap.datos[indiceHijo]
+		indice = indiceHijo
+	}
+}
+
+func (heap *heap[T]) redimensionar(tam int) {
+	nuevoArr := make([]T, tam)
+	copy(nuevoArr, heap.datos)
+	heap.datos = nuevoArr
+}
+
+//Hacer HeapSort
--- tdas/pila/pila_dinamica.go
+++ tdas/pila/pila_dinamica.go
@@ -1,10 +1,10 @@
 package pila
 
 const (
-	TAM_INICIAL                   = 6
-	redimensionarAumento          = 2
-	redimensionarDisminucion      = 2
-	multiplicadorReducirCapacidad = 4
+	TAM_INICIAL                    = 6
+	FACTOR_REDIMENSION_AUMENTO     = 2
+	FACTOR_REDIMENSION_DISMINUCION = 2
+	MULTIPLICADOR_REDUCIR_CANTIDAD = 4
 )
 
 // Definición del struct pila proporcionado por la cátedra.
@@ -39,7 +39,7 @@ func (pila *pilaDinamica[T]) VerTope() T {
 // Apilar agrega un nuevo elemento a la pila.En caso de que la cantidad de elementos sea igual a la capacidad del slice, se llama a la funcion redimensionar para asi duplicar la capacidad de este.
 func (pila *pilaDinamica[T]) Apilar(valor T) {
 	if pila.cantidad == len(pila.datos) {
-		pila.redimensionar(len(pila.datos) * redimensionarAumento)
+		pila.redimensionar(len(pila.datos) * FACTOR_REDIMENSION_AUMENTO)
 	}
 	pila.datos[pila.cantidad] = valor
 	pila.cantidad++
@@ -52,8 +52,8 @@ func (pila *pilaDinamica[T]) Desapilar() T {
 	}
 	valor := pila.datos[pila.cantidad-1]
 	pila.cantidad--
-	if (!pila.EstaVacia()) && ((pila.cantidad * multiplicadorReducirCapacidad) <= len(pila.datos)) {
-		pila.redimensionar(len(pila.datos) / redimensionarDisminucion)
+	if (!pila.EstaVacia()) && ((pila.cantidad * FACTOR_REDIMENSION_DISMINUCION) <= len(pila.datos)) {
+		pila.redimensionar(len(pila.datos) / MULTIPLICADOR_REDUCIR_CANTIDAD)
 	}
 	return valor
 }
@@ -63,5 +63,4 @@ func (pila *pilaDinamica[T]) redimensionar(tam int) {
 	nuevosDatos := make([]T, tam)
 	copy(nuevosDatos, pila.datos)
 	pila.datos = nuevosDatos
-
 }
