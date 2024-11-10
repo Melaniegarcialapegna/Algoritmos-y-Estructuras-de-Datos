f657f4850c84276bb6da05b33f7b8e6c5c6574c0
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2024-11-10 02:41:48 -0300

    Nos faltan mas ejs

--- Extras/Guia Grafos/ej18Mel.py
+++ Extras/Guia Grafos/ej18Mel.py
@@ -1,12 +1,12 @@
 import collections
 
-def encontrar_minimos(x,y):
-    distanciaCaminoMinimo = 0
-    distancia = {}
+def encontrarCaminoMinimo(x,y):
+    nuevosVertices = {} #NuevosVertices va a tener el numero multiplicado/restado y como dato la distancia
     if x == y:
-        return distanciaCaminoMinimo
+        return 0
     
     cola= collections.deque()
+    nuevosVertices[x] = 0
     cola.append(x)
 
     while len(cola) != 0:
@@ -14,14 +14,19 @@ def encontrar_minimos(x,y):
 
         #Si multiplico x2
         proxMult = vertice * 2
-        if  proxMult not in distancia:
-            distancia[proxMult] = distancia[vertice] +1
+        if  proxMult not in nuevosVertices:
+            nuevosVertices[proxMult] = nuevosVertices[vertice] +1
             cola.append(proxMult)
 
         #Si resto 1
-        proxResta = vertice -1
-        if  proxResta not in distancia:
-            distancia[proxResta] = distancia[vertice] +1
-            cola.append(proxResta)
+        if vertice > 1: # si no me rompe xd
+            proxResta = vertice -1
+            if  proxResta not in nuevosVertices:
+                nuevosVertices[proxResta] = nuevosVertices[vertice] +1
+                cola.append(proxResta)
 
-
+        #Me fijo si encontre a Y
+        if y in nuevosVertices:
+            return nuevosVertices[y]
+        
+    return -1
--- Extras/Guia Grafos/ej18Mir.py
+++ Extras/Guia Grafos/ej18Mir.py
@@ -1,12 +1,14 @@
-def encontrarCamino(x, y, distancia):
+def encontrarCamino(x, y, anterior, distancia):
 	if x == y :
 		return distancia
 
 	izq = x - 1
 	der = 2 * x
 
-	distIzq = encontrarCamino(izq, y, distancia+1)
-	distDer = encontrarCamino(der, y, distancia+1)
+	if abs(y-izq) < abs(y-x):
+		distIzq = encontrarCamino(izq, y, x, distancia+1) 
+	if abs(y-der) < abs(y-x):
+		distDer = encontrarCamino(der, y, x, distancia+1)
 
 	if distIzq < distDer:
 		return distIzq
