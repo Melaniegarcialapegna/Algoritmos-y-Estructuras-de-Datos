6240c8c4e6fd7ae075afc2feee9ee172cf52cfad
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2024-11-19 20:05:44 -0300

    juj

---
+++ Entregas/tp3/grafo.py
@@ -0,0 +1,26 @@
+
+#Implementacion de TDA Grafo
+
+class Grafo:
+    def __init__(self,es_dirigido,vertices):
+        #Lo hacemos con dicc de dicc??
+
+
+    # Primitivas
+    def agregar_vertice(self,vertice)
+
+    def borrar_vertice(self,vertice)
+
+    def agregar_arista(self,vertice,adyacente,peso)
+
+    def borrar_arista(self,vertice,adyacente)
+
+    def estan_unidos(self,vertice,adyacente)
+
+    def peso_arista(self,vertice,adyacente)
+
+    def obtener_vertices(self) -> devuelve una lista con todos los vertices del grafo
+
+    def vertice_aleatorio(self)
+
+    def adyacentes(self,vertice)
