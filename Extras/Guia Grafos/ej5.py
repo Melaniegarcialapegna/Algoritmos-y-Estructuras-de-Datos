d9a12b3b675ae250dbe1b97f30bd4fb8617f0855
mirkosaenz <saenzvalientemirko@gmail.com>
2024-11-26 21:58:25 -0300

    AL FIN

--- Extras/Guia Grafos/ej5.py
+++ Extras/Guia Grafos/ej5.py
@@ -1,32 +1,52 @@
 def encontrar_ciclo(grafo):
+    '''
+    Devuelve una lista de vertices que conforman el ciclo. 
+    Si no hay ciclo, debe devolver None. 
+    '''
+
     padres = {}
-    visitados = {}
+    visitados = set()
+    orden = {}
+
     for vertice in grafo:
         if vertice not in visitados:
+            iteracion_actual = set()
+            iteracion_actual.add(vertice)
+            visitados.add(vertice)
             padres[vertice] = None
-            visitados[vertice] = True
-            ciclo = ciclo_dfs(grafo, vertice,padres,visitados)
+            orden[vertice] = 0
+
+            ciclo = dfs(grafo, vertice, visitados, padres, orden, iteracion_actual)
             if ciclo is not None:
                 return ciclo
+
     return None
 
-def ciclo_dfs(grafo, vertice, padres, visitados):
+def dfs(grafo, vertice, visitados, padres, orden, iteracion_actual):
     for adyacente in grafo.adyacentes(vertice):
-        if adyacente in visitados:
-            return recrear_ciclo(padres,vertice,adyacente)            
-        else:
-            padres[adyacente] = vertice
-            visitados[adyacente] = True
-            ciclo =  ciclo_dfs(grafo, adyacente, padres, visitados)
-            if ciclo is not None:
-                return ciclo
+        if adyacente in visitados and adyacente in iteracion_actual:
+            if orden[adyacente] < orden[vertice]:
+                return reconstruir_ciclo(padres, adyacente, vertice)
+
+        orden[adyacente] = orden[vertice] + 1
+        visitados.add(adyacente)
+        iteracion_actual.add(adyacente)
+        padres[adyacente] = vertice
+        ciclo = dfs(grafo, adyacente, visitados, padres, orden, iteracion_actual)
+        if ciclo is not None:
+            return ciclo
+    
     return None
 
-            
-def recrear_ciclo(padres, vertice, adyacente):
+def reconstruir_ciclo(padres, origen, final):
+    actual = final
     ciclo = []
-    while adyacente != vertice:
-        ciclo.append(vertice)
-        vertice = padres[vertice]
-    ciclo.append(vertice)
-    return ciclo[::-1]  
+
+    while actual != origen:
+        ciclo.append(actual)
+        actual = padres[actual]
+    ciclo.append(origen)
+
+    ciclo = ciclo[::-1]
+
+    return ciclo
